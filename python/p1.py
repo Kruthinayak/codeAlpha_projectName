95c4081c6fc11a83198fe6666a33631ca08a7697
Kruthi <kruthinayaka07@gmail.com>
2025-09-08 22:43:42 +0530

    Delete python/p1.py

--- python/p1.py
+++
@@ -1,6 +0,0 @@
-boy_name=input("Enter the name of the boy: ")
-boy_age = int(input("Enter the age of the boy: "))
-girl_name=input("Enter the name of the girl: ")
-girl_age=int(input("Enter the age of the girls: "))
-age_diff= boy_age - girl_age 
-print(f"{boy_name} loves {girl_name} and their age is {age_diff}")
